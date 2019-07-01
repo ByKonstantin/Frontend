25617f16a223948156b93bd9ec2dd33c62113f5a
ByKonstantin <51529177+ByKonstantin@users.noreply.github.com>
2019-07-01 04:33:19 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,16 @@
+$(function() {
+	
+	    var swiper = new Swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+
+	// Custom JS
+
+});
